26de1e5451e6ccb4868f54e97feef8f8299085e4
Tucker Nemcek <tuckernemcek21@gmail.com>
2019-04-10 09:46:14 -0600

    api is making calls and recieving data

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import Titles from './components/Titles'
 import Form from './components/Form'
 import Weather from './components/Weather'
 
+const weatherAPI_KEY = '63bff92f1ebd113efe2539601d47425c'
+
 class App extends React.Component {
+
+  getWeather = async (e) => {
+    e.preventDefault()
+    const weatherAPI_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${weatherAPI_KEY}`)
+
+    const data = await weatherAPI_call.json()
+    console.log(data)
+  }
+
   render() {
     return (
 
       <div>
       <Titles />
-      <Form />
+      <Form getWeather = {this.getWeather}/>
+      <Weather />
       </div>
     )
   }
