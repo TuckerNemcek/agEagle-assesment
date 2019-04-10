b93f7dcafadd7e69723e312b7782864fc7bc10f3
Tucker Nemcek <tuckernemcek21@gmail.com>
2019-04-10 12:23:33 -0600

    api is returning correct data

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import Weather from './components/Weather'
 
 const weatherAPI_KEY = '63bff92f1ebd113efe2539601d47425c'
 
+
 class App extends React.Component {
 
   getWeather = async (e) => {
     e.preventDefault()
-    const weatherAPI_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${weatherAPI_KEY}`)
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+    const weatherAPI_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Montreal&mode=json&appid=${weatherAPI_KEY}&units=metric`)
+  //  const weatherAPI_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=London,uk,&appid=$${weatherAPI_KEY}`)
 
     const data = await weatherAPI_call.json()
+  //  const data = await weatherAPI_
     console.log(data)
   }
 
