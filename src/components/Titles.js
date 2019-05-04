bfb7d51b3314f5201e964fb2c57039317fc9fbea
TuckerNemcek <tuckernemcek21@gmail.com>
2019-05-04 16:31:24 -0600

    updated text

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,7 +4,7 @@ import React from "react";
 const Titles = () => (
 	<div>
 		<h1 className="title-container__title">A map site made by Tucker Nemcek</h1>
-		<h3 className="title-container__subtitle">I enjoy working with map APIs. This was just a fun project to make a good looking map site. It's interactive, feel free to scrol and drag your way around! </h3>
+		<h3 className="title-container__subtitle">I enjoy working with map APIs. This was just a fun project to make a good looking map site. It's interactive, feel free to scroll and drag your way around! </h3>
 	</div>
 );
 
